f55d86f2dbd55fa74eddfbcbc4b8c6f68f2fe3ce
Corentinfaye <Corentin.faye@chartes.psl.eu>
2019-07-18 16:00:06 +0200

    Add flaskapp to create easily file for creation of manifest

---
+++ flaskapp.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, Response
+import json
+app = Flask(__name__, static_folder="./static", template_folder="./templates")
+
+
+@app.route("/", methods=["POST", "GET"])
+def form():
+    if request.method == "POST":
+        data = request.form
+        for key, value in data.items():
+            if key == "Name of Corpus":
+                dict = {value:[]}
+                name_c = value
+                first = True
+            elif "title" in key:
+                if first == False:
+                    dict[name_c].append(dict_i)
+                first = False
+                dict_i = {"title": "",
+                          "codex_name":"",
+                          "manifest_link":"",
+                          "images":{}
+                          }
+                dict_i.update({"title":value})
+            elif "codex" in key:
+                dict_i.update({"codex_name":value})
+            elif "manifest" in key:
+                dict_i.update({"manifest_link":value})
+            elif "folio_name" in key:
+                label_image = value
+            elif "folio_link" in key:
+                dict_i["images"].update({label_image:value})
+        dict[name_c].append(dict_i)
+        return Response(response=json.dumps(dict), mimetype="dict/json",headers={"Content-disposition":"attachment; filename=" + name_c + ".json"})
+    return render_template("formulaire.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
